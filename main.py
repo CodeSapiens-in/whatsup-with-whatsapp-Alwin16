192bc17874bcde63710523815de7416671f901a0
Alwin16 <141432912+Alwin16@users.noreply.github.com>
2023-10-02 16:41:00 +0000

    after addition

--- main.py
+++ main.py
@@ -7,5 +7,44 @@ with open(r"_chat.txt", 'r') as fp:
             pollcount=pollcount+1
     print('Total Number of lines:', lines)
     print('Total polls: '+str(pollcount))
-    print('dummy msgs')
+    import re
+    deletedmsgcount=0
+    Alwinmsg=-1; #since name will be shown once while joining
+    message_edited=0
+    d={}
+    
+    url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    pattern = r'\[([\d/]+, [\d:]+ [APMapm]+)\] (.+?): (.+)'
+    link_count=0
+    for line in text:
+        if re.search(url_pattern, line):
+            link_count += 1
+
+        if "This message was deleted" in line:
+            deletedmsgcount+=1
+        if "A7WiN✨:" in line:
+            Alwinmsg+=1
+        if "<This message was edited>" in line:
+            message_edited+=1
+        
+        match = re.match(pattern, line)
+        if match:
+            parts = line.split('] ')
+            if len(parts) == 2:
+                timestamp, rest = parts
+                sender, message = rest.split(': ')
+                print(sender)
+                if sender not in d:
+                    d[sender]=1
+                else:
+                    d[sender]+=1
+
+
+    print("The total number of messages deleted were:-",deletedmsgcount)
+    print("Alwin messaged:-",Alwinmsg,"times")
+    print("The number of messages that was edited were:-",message_edited)
+    print("Number of links:-",link_count)
+    for i,j in d.items():
+        print(i,j)
+    
     
